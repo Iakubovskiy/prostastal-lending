3bc3b26c0a36077830dddd6e87a592126cc403d4
Iakuba <diman.150106@gmail.com>
2025-05-10 02:10:01 +0300

    trying to add og meta data, attempt 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-      react(),
-        tailwindcss(),
-    ],
+export default defineConfig(() => {
+    const lang = process.env.LANG || 'en'
+    return {
+        plugins: [
+            react(),
+            tailwindcss(),
+        ],
+        build: {
+            outDir: `dist-${lang}`,
+            emptyOutDir: true,
+            rollupOptions: {
+                input: resolve(__dirname, `buildLocales/${lang}/index.html`)
+            }
+        }
+    }
 })
